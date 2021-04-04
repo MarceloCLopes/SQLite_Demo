723e04db21871947989e941d70c24ca0e70b0372
MarceloCLopes <marc01mcl@gmail.com>
2021-04-04 12:13:21 -0300

    implemented methods database insert main activity file

--- app/src/main/java/com/mcl/sqlitedemo/MainActivity.kt
+++ app/src/main/java/com/mcl/sqlitedemo/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.mcl.sqlitedemo
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helper = MyDBHelper(applicationContext)
+        val db = helper.readableDatabase
+        val rs = db.rawQuery("SELECT * FROM USERS", null)
+
+        if (rs.moveToNext()){
+            Toast.makeText(applicationContext, rs.getString(1), Toast.LENGTH_LONG).show()
+        }
+
+        btnInsert.setOnClickListener {
+            val contentValue = ContentValues()
+            contentValue.put("USERNAME", editTextEmail.text.toString())
+            contentValue.put("PASSWORD", editTextPassword.text.toString())
+            db.insert("USERS", null, contentValue)
+
+            editTextEmail.setText("")
+            editTextPassword.setText("")
+            editTextEmail.requestFocus()
+        }
     }
 }
